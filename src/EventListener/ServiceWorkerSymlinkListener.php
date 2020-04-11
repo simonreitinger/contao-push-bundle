71ff4172197490078e702369640aca659d80e437
Simon Reitinger <simonreitinger@tuta.io>
2020-04-12 00:05:41 +0200

    Symlink sw.js to web root

--- src/EventListener/ServiceWorkerSymlinkListener.php
+++ src/EventListener/ServiceWorkerSymlinkListener.php
@@ -1,8 +1,20 @@
 <?php
 
+declare(strict_types=1);
+
+/*
+ * This file is part of the Contao Push Bundle.
+ * (c) Werbeagentur Dreibein GmbH
+ */
+
 namespace SimonReitinger\ContaoPushBundle\EventListener;
 
-class ServiceWorkerSymlinkListener
-{
+use Contao\CoreBundle\Event\GenerateSymlinksEvent;
 
+final class ServiceWorkerSymlinkListener
+{
+    public function onGenerateSymlinks(GenerateSymlinksEvent $event): void
+    {
+        $event->addSymlink('web/bundles/contaopush/sw.js', 'web/contao-push-sw.js');
+    }
 }
