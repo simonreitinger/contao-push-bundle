3f148a331719395cc94b670967e947a5bc42567c
Simon Reitinger <simonreitinger@tuta.io>
2020-04-12 00:05:13 +0200

    Refactor News, add URL

--- src/DataContainer/News.php
+++ src/DataContainer/News.php
@@ -1,9 +1,16 @@
 <?php
 
+declare(strict_types=1);
+
+/*
+ * This file is part of the Contao Push Bundle.
+ * (c) Werbeagentur Dreibein GmbH
+ */
+
 namespace SimonReitinger\ContaoPushBundle\DataContainer;
 
-use Contao\DataContainer;
-use Contao\Model;
+use Contao\Controller;
+use Contao\CoreBundle\Framework\ContaoFramework;
 use Contao\NewsModel;
 use SimonReitinger\ContaoPushBundle\Push\PushManager;
 use Symfony\Component\HttpFoundation\RequestStack;
@@ -21,36 +28,42 @@ class News
     private $requestStack;
 
     /**
+     * @var ContaoFramework
+     */
+    private $framework;
+
+    /**
      * News constructor.
+     *
      * @param PushManager $manager
      */
-    public function __construct(PushManager $manager, RequestStack $requestStack)
+    public function __construct(PushManager $manager, RequestStack $requestStack, ContaoFramework $framework)
     {
         $this->manager = $manager;
         $this->requestStack = $requestStack;
+        $this->framework = $framework;
     }
 
-    public function onLoad($dc)
+    public function onLoad($dc): void
     {
         $request = $this->requestStack->getCurrentRequest();
         if (!$request->query->get('sendPush')) {
             return;
         }
 
-        $model = $this->getModel($dc->id, $dc->table);
+        $this->framework->initialize();
 
-        if (!$model) {
-            return;
-        }
+        $adapter = $this->framework->getAdapter(NewsModel::class);
+        $model = $adapter->findByPk($dc->id);
 
         $title = $model->headline;
         $body = $model->subheadline;
+        $url = sprintf(
+            '%s/%s',
+            $request->getSchemeAndHttpHost(),
+            Controller::replaceInsertTags(sprintf('{{news_url::%s}}', $dc->id))
+        );
 
-        $this->manager->sendNotification($title, $body);
-    }
-
-    public function getModel($id, $table)
-    {
-        return (Model::getClassFromTable($table))::findByPk($id);
+        $this->manager->sendNotification($title, $body, $url);
     }
 }
